ec70389ba4f97d2e946d1de84a0ffd82f8ec3aeb
Júlio <jcsme.snf24@uea.edu.br>
2025-12-04 22:18:47 -0300

    feat(database): substituir SQLAlchemy por psycopg2 em toda camada de acesso ao banco

--- backend/backend/routers/upload.py
+++ backend/backend/routers/upload.py
@@ -1,31 +1,16 @@
-# app/routes/upload.py
-from fastapi import APIRouter, UploadFile, Depends
-from sqlalchemy.orm import Session
-from fastapi import HTTPException
-from database.base import SessionLocal
-from utils.file_validation import validate_upload_file
-from services.parser_service import process_and_insert
+from fastapi import APIRouter, UploadFile, Depends, HTTPException
+from backend.database.base import get_db
+from backend.utils.file_validation import validate_upload_file
+from backend.services.parser_service import process_and_insert
 
 router = APIRouter(prefix="/upload", tags=["upload"])
 
-# conexão com o PostgreSQL por request
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-# recebe o arquivo enviado pelo usuário
-@router.post("/", summary="Upload e processamento de CSV/XLSX")
-def upload_file(file: UploadFile, db: Session = Depends(get_db)):
+@router.post("/", summary="Upload e processamento CSV/XLSX")
+def upload_file(file: UploadFile, db = Depends(get_db)):
     if not file:
         raise HTTPException(status_code=400, detail="Arquivo não enviado.")
 
-    # Validação básica (extensão, tamanho)
     validate_upload_file(file)
 
-    # processa e insere
     result = process_and_insert(file, db)
     return result
