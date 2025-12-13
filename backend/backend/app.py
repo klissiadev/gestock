eac4aa6841e626f9e3970e9a3493c5f8a84b42c1
Júlio <jcsme.snf24@uea.edu.br>
2025-12-13 02:41:35 -0300

    feat(schemas): Corrigir campos de importação

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -9,12 +9,19 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://localhost:3000", "http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 app.include_router(upload_service)
 app.include_router(mail_service)
 app.include_router(produto_router)
+app.include_router(movimentacao_router)
