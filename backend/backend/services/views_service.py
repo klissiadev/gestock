c20521ef4f19f847e38182e45b353570402dece7
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2025-12-14 11:46:13 -0300

    Fix(view-service): gitignore funcionando adequadamente

---
+++ backend/backend/services/views_service.py
@@ -0,0 +1,6 @@
+# service/viewws_service
+"""
+    Serviço responsável por buscar Views armazenadas no banco de dados
+    Para melhor desempenho -> Ainda em duvida sobre sistema de cache ou materialized view
+
+"""
