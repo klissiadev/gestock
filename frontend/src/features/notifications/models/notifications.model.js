e96ad3f3be17fc1cd1385511e005e807e954f514
klissia <akfma.snf24@uea.edu>
2026-01-12 12:12:17 -0400

    feat(notification): criar estrutura inicial de notificações

---
+++ frontend/src/features/notifications/models/notifications.model.js
@@ -0,0 +1,17 @@
+
+import { NOTIFICATION_TYPE } from "../constants/notificationType";
+import { NOTIFICATION_SEVERITY } from "../constants/notificationSeverity";
+
+/**
+ * @typedef {Object} Notification
+ * @property {string} id
+ * @property {string} type
+ * @property {string} severity
+ * @property {string} title
+ * @property {string} message
+ * @property {Object} reference
+ * @property {string} reference.type
+ * @property {string} reference.id
+ * @property {boolean} read
+ * @property {string} createdAt
+ */
