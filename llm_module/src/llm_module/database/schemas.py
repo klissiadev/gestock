e8e3eeb3064bedb5c7f78b1a62045cde6c559c7c
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-01-13 18:34:23 -0300

    fix(run_chatbot): corrigido para ser capaz de saber o nome do usuario do PC

--- llm_module/src/llm_module/database/schemas.py
+++ llm_module/src/llm_module/database/schemas.py
@@ -52,83 +52,35 @@ IMPORT_SCHEMAS = {
 # -------------------------
 # PRODUTO
 # -------------------------
-class ProdutoBase(BaseModel):
+class Produto(BaseModel):
     nome: str
     descricao: Optional[str] = None
     estoque_minimo: int
     data_validade: Optional[date] = None
     ativo: bool = True
 
-class ProdutoCreate(ProdutoBase):
-    pass
-
-class ProdutoUpdate(BaseModel):
-    nome: Optional[str] = None
-    descricao: Optional[str] = None
-    estoque_minimo: Optional[int] = None
-    data_validade: Optional[date] = None
-    ativo: Optional[bool] = None
-
-class ProdutoOut(ProdutoBase):
-    id: int
-    created_at: datetime
-    updated_at: Optional[datetime]
-
 # -------------------------
 # MOVIMENTAÇÃO de entrada
 # -------------------------
-class MovimentacaoEntradaBase(BaseModel):
+class MovimentacaoEntrada(BaseModel):
     produto_id: int
     quantidade: int
     data_de_compra: date
     preco_de_compra: float
     fornecedor: str
 
-class MovimentacaoEntradaCreate(MovimentacaoEntradaBase):
-    pass
-
-class MovimentacaoEntradaOut(MovimentacaoEntradaBase):
-    id: int
-    created_at: datetime
-
 # -------------------------
 # MOVIMENTAÇÃO de saida
 # -------------------------
-class MovimentacaoSaidaBase(BaseModel):
+class MovimentacaoSaida(BaseModel):
     produto_id: int
     quantidade: int
     data_de_venda: date
     preco_de_venda: float
     cliente: str
 
-class MovimentacaoSaidaCreate(MovimentacaoSaidaBase):
-    pass
-
-class MovimentacaoSaidaOut(MovimentacaoSaidaBase):
-    id: int
-    created_at: datetime
-
 # -------------------------
-# LOG DE IMPORTAÇÃO
+# REQUEST DA LLM
 # -------------------------
-class LogImportacaoBase(BaseModel):
-    nome_arquivo: str
-    qntd_registros: int
-    data_importacao: date
-    status: str
-    msg_erro: Optional[str] = None
-    id_usuario: int
-
-class LogImportacaoCreate(LogImportacaoBase):
-    pass
-
-class LogImportacaoUpdate(BaseModel):
-    nome_arquivo: Optional[str] = None
-    qntd_registros: Optional[int] = None
-    data_importacao: Optional[date] = None
-    status: Optional[str] = None
-    msg_erro: Optional[str] = None
-    id_usuario: Optional[int] = None
-
-class LogImportacaoOut(LogImportacaoBase):
-    id_log_importacao: int
+class LLMRequest(BaseModel):
+    question: str
