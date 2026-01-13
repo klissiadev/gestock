f7e156d4a17819e4e1dbaf21b8d20e4eb8755f95
Ana Klissia Furtado Martins <akfma.snf24@uea.edu.br>
2026-01-12 21:42:44 -0400

    refactor(notification): ajustar estrutura de eventos e notificações

---
+++ frontend/src/features/notifications/models/notificationsEvents.js
@@ -0,0 +1,49 @@
+import { NOTIFICATION_TYPE } from "../constants/notificationType";
+
+/**
+ * Modelo de evento bruto (fato do sistema).
+ * Esse model NÃO tem título, mensagem ou severidade.
+ * Ele será normalizado depois.
+ */
+
+/**
+ * @typedef {Object} NotificationEvent
+ *
+ * @property {string} id
+ * @property {NOTIFICATION_TYPE} type
+ *
+ * @property {Object} context
+ * @property {Object} context.flags
+ *
+ * // Ruptura / estoque
+ * @property {boolean} [context.flags.isBelowMinimum]
+ * @property {boolean} [context.flags.isNearMinimum]
+ *
+ * // Validade
+ * @property {boolean} [context.flags.isExpired]
+ * @property {boolean} [context.flags.isNearExpiration]
+ *
+ * // Sucesso
+ * @property {boolean} [context.flags.importCompleted]
+ * @property {boolean} [context.flags.stockUpdated]
+ *
+ * // Erro
+ * @property {boolean} [context.flags.hasInconsistency]
+ * 
+ * // Sugestão
+ * @property {boolean} [context.flags.suggestReplenishment]
+ *
+ * @property {Object} [context.data]
+ * @property {number} [context.data.currentStock]
+ * @property {number} [context.data.minimumStock]
+ * @property {string} [context.data.expirationDate]
+ *
+ * @property {Object} reference
+ * @property {"PRODUCT" | "STOCK" | "IMPORT"} reference.type
+ * @property {string} reference.id
+ *
+ * @property {string} createdAt
+ * @property {string} userId
+ */
+
+export {};
