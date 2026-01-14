f8a28245b07be0a88692370adc936d87dbeadbb4
klissia <akfma.snf24@uea.edu>
2026-01-14 12:22:35 -0400

    feat(notification):configurando backend

--- frontend/src/features/notifications/services/notificationNormalizer.js
+++ frontend/src/features/notifications/services/notificationNormalizer.js
@@ -27,13 +27,28 @@ function baseNotification({
   };
 }
 
+// função para calcular número de dias
+function daysUntil(dataAlvo) {
+  const today = new Date();
+  const target = new Date(dataAlvo);
+
+  // Zera horas para evitar erro de fuso/horário
+  today.setHours(0, 0, 0, 0);
+  target.setHours(0, 0, 0, 0);
+
+  const diffMs = target - today;
+  const diffDias = Math.ceil(diffMs / (1000 * 60 * 60 * 24));
+
+  return diffDias;
+}
+
 export function normalizeNotification(event) {
   const { type, context, reference, createdAt, userId, id } = event;
   const { flags, data } = context;
 
   // RUPTURA
   if (type === "RUPTURE") {
-    if (flags.isBelowMinimum) {
+    if (event.context.state === "BELOW_MINIMUM") {
       return baseNotification({
         id,
         type,
@@ -46,7 +61,7 @@ export function normalizeNotification(event) {
       });
     }
 
-    if (flags.isNearMinimum) {
+    if (event.context.state === "ISNEAR_MINIMUM") {
       return baseNotification({
         id,
         type,
@@ -61,26 +76,26 @@ export function normalizeNotification(event) {
   }
   //VALIDADE 
   if (type === "VALIDITY"){
-    if(flags.isExpired){
+    if(event.context.state === "IS_EXPIRED"){
         return baseNotification({
             id,
             type,
             severity: NOTIFICATION_SEVERITY.CRITICAL,
             title: "Lote vencido",
-            message: `O produto ${reference.id} está vencido desde o dia ${data?.expirationDate}.`,
+            message: `O produto ${reference.id} está vencido há ${(-1)*daysUntil(data?.expirationDate)} dias.`,
             reference,
             createdAt,
             userId
         })
     }
 
-    if(flags.isNearExpiration){
+    if(event.context.state === "ISNEAR_EXPIRATION"){
         return baseNotification({
             id,
             type,
             severity: NOTIFICATION_SEVERITY.WARNING,
             title: "Lote próximo do vencimento",
-            message: `O produto ${reference.id} vencerá dia ${data?.expirationDate}.`,
+            message: `O produto ${reference.id} vencerá em ${daysUntil(data?.expirationDate)} dias.`,
             reference,
             createdAt,
             userId
@@ -88,7 +103,7 @@ export function normalizeNotification(event) {
     }
   }
   //SUCCESS
-  if (type === "VALIDITY" && flags.importCompleted && flags.stockUpdated){
+  if (type === "VALIDITY" && event.context.state === "STOCK_UPDATED"){
         return baseNotification({
             id,
             type,
@@ -114,7 +129,7 @@ export function normalizeNotification(event) {
         })
     }
     //SUGGESTION
-    if(type === "SUGGESTION" && flags.suggestReplenishment){
+    if(type === "SUGGESTION" && event.context.state === "SUGGEST_REPLENISHMENT"){
         return baseNotification({
             id,
             type,
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.jsx'
 
-import "./features/notifications/notificationNormalizer.test.js";
+import "./tests/testNotificationService.js";
 
 
 createRoot(document.getElementById('root')).render(
