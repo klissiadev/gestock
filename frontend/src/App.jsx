7f1003290f5e3acdb510b5aa41afa84366a97ab0
klissia <akfma.snf24@uea.edu>
2026-01-19 12:45:13 -0400

    feat(notification): endpoint da API do frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import NotificationToastTester from "./features/notifications/components/NotificationItem";
 
 function App() {
   return (
     <BrowserRouter>
+      <NotificationToastTester />
       <AppRoutes />
     </BrowserRouter>
   )
 }
-
 export default App;
+
