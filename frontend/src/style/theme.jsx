156ddde3dd514f37c58d3a280115d614516f7ea6
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-01-27 11:29:59 -0400

    style(theme): Criado estrutura de estilo modular

---
+++ frontend/src/style/theme.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+import { palette } from './pallette';
+
+export const theme = createTheme({
+    // Colocar aqui os componentes
+    palette
+
+});
+
+// Tutorial
+// Usando as cores da Paleta
+
+// color=theme.palette.table.main
+// theme.palette.iconButton.main
