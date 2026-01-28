9e5e3e4b326bad2e910ac149ef834a2ddb8e372f
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-01-28 11:01:28 -0400

    feat(datagrid): Persoalizando datagrid

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import StockSheets from '../features/stock/pages/StockSheets'
 import UploadPage from '../features/upload/pages/UploadPage'
 import NotificationPage from '../features/notifications/NotificationPage'
 import HomePage from '../features/home/HomePage'
+import StockPage from '../features/stock/StockPage'
 
 
 const AppRoutes = () => {
@@ -13,6 +14,7 @@ const AppRoutes = () => {
         <Route path='/upload' element={<UploadPage />}/>
         <Route path='/sheets' element={<StockSheets />}/>
         <Route path='/notifications' element={<NotificationPage/>}/>
+        <Route path='/debug' element={<StockPage />} />
     </Routes>
   )
 }
