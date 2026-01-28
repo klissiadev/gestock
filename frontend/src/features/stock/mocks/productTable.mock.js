d7b6e3899b723ca97eddb5e13d6aade0779651d9
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-01-28 11:01:28 -0400

    feat(datagrid): Persoalizando datagrid

---
+++ frontend/src/features/stock/mocks/productTable.mock.js
@@ -0,0 +1,69 @@
+export const products = [
+    {
+        'nome_produto': 'Cabo USB montado', 
+        'descricao': 'Produto semiacabado resultante de montagem parcial: Cabo USB montado.', 
+        'estoque_atual': 0, 
+        'estoque_minimo': 54, 
+        'ativo': true, 
+        'data_validade': '2026-09-04'
+    },
+
+    {'nome_produto': 'Carcaça do headset montada', 'descricao': 'Produto semiacabado resultante de montagem parcial: Carcaça do headset montada.', 'estoque_atual': 0, 'estoque_minimo': 80, 'ativo': true, 'data_validade': '2026-12-31'},
+
+    {'nome_produto': 'Carcaça do mouse injetada', 'descricao': 'Produto semiacabado resultante de montagem parcial: Carcaça do mouse injetada.', 'estoque_atual': 0, 'estoque_minimo': 54, 'ativo': true, 'data_validade': '2026-08-19'},
+
+    {'nome_produto': 'Carcaça do teclado injetada', 'descricao': 'Produto semiacabado resultante de montagem parcial: Carcaça do teclado injetada.', 'estoque_atual': 0, 'estoque_minimo': 81, 'ativo': true, 'data_validade': '2026-08-30'},
+
+    {'nome_produto': 'Conjunto de botões do mouse', 'descricao': 'Produto semiacabado resultante de montagem parcial: Conjunto de botões do mouse.', 'estoque_atual': 0, 'estoque_minimo': 113, 'ativo': true, 'data_validade': '2026-11-09'},
+
+    {'nome_produto': 'Conjunto de switches mecânicos', 'descricao': 'Produto semiacabado resultante de montagem parcial: Conjunto de switches mecânicos.', 'estoque_atual': 0, 'estoque_minimo': 96, 'ativo': true, 'data_validade': '2027-01-14'},
+
+    {'nome_produto': 'Headset Bluetooth', 'descricao': 'Produto acabado pronto para venda ao consumidor: Headset Bluetooth.', 'estoque_atual': -136, 'estoque_minimo': 50, 'ativo': true, 'data_validade': '2026-06-12'},
+
+    {'nome_produto': 'Headset com circuito montado', 'descricao': 'Produto semiacabado resultante de montagem parcial: Headset com circuito montado.', 'estoque_atual': 0, 'estoque_minimo': 52, 'ativo': true, 'data_validade': '2026-12-17'},
+
+    {'nome_produto': 'Headset com microfone', 'descricao': 'Produto acabado pronto para venda ao consumidor: Headset com microfone.', 'estoque_atual': 0, 'estoque_minimo': 56, 'ativo': true, 'data_validade': '2026-05-02'},
+
+    {'nome_produto': 'Headset gamer', 'descricao': 'Produto acabado pronto para venda ao consumidor: Headset gamer.', 'estoque_atual': -314, 'estoque_minimo': 31, 'ativo': true, 'data_validade': '2026-06-27'},
+
+    {'nome_produto': 'Headset profissional', 'descricao': 'Produto acabado pronto para venda ao consumidor: Headset profissional.', 'estoque_atual': 0, 'estoque_minimo': 30, 'ativo': true, 'data_validade': '2026-04-25'},
+
+    {'nome_produto': 'Kit teclado + mouse', 'descricao': 'Produto acabado pronto para venda ao consumidor: Kit teclado + mouse.', 'estoque_atual': 0, 'estoque_minimo': 26, 'ativo': true, 'data_validade': '2026-07-10'},
+
+    {'nome_produto': 'Mouse corporativo', 'descricao': 'Produto acabado pronto para venda ao consumidor: Mouse corporativo.', 'estoque_atual': 0, 'estoque_minimo': 21, 'ativo': true, 'data_validade': '2026-07-13'},
+
+    {'nome_produto': 'Mouse ergonômico', 'descricao': 'Produto acabado pronto para venda ao consumidor: Mouse ergonômico.', 'estoque_atual': 0, 'estoque_minimo': 27, 'ativo': true, 'data_validade': '2026-07-12'},
+
+    {'nome_produto': 'Mouse gamer RGB', 'descricao': 'Produto acabado pronto para venda ao consumidor: Mouse gamer RGB.', 'estoque_atual': -259, 'estoque_minimo': 50, 'ativo': true, 'data_validade': '2026-07-02'},
+
+    {'nome_produto': 'Mouse sem fio', 'descricao': 'Produto acabado pronto para venda ao consumidor: Mouse sem fio.', 'estoque_atual': -135, 'estoque_minimo': 58, 'ativo': true, 'data_validade': '2026-06-03'},
+
+    {'nome_produto': 'Mouse óptico', 'descricao': 'Produto acabado pronto para venda ao consumidor: Mouse óptico.', 'estoque_atual': -258, 'estoque_minimo': 23, 'ativo': true, 'data_validade': '2026-04-22'},
+    
+    {'nome_produto': 'Módulo eletrônico testado', 'descricao': 'Produto semiacabado resultante de montagem parcial: Módulo eletrônico testado.', 'estoque_atual': 0, 'estoque_minimo': 97, 'ativo': true, 'data_validade': '2026-08-17'},
+    
+    {'nome_produto': 'Módulo final inspecionado', 'descricao': 'Produto semiacabado resultante de montagem parcial: Módulo final inspecionado.', 'estoque_atual': 0, 'estoque_minimo': 119, 'ativo': true, 'data_validade': '2027-01-12'},
+    
+    {'nome_produto': 'Módulo wireless montado', 'descricao': 'Produto semiacabado resultante de montagem parcial: Módulo wireless montado.', 'estoque_atual': 0, 'estoque_minimo': 76, 'ativo': true, 'data_validade': '2026-10-12'},
+    
+    {'nome_produto': 'Placa de LED RGB montada', 'descricao': 'Produto semiacabado resultante de montagem parcial: Placa de LED RGB montada.', 
+    'estoque_atual': 0, 'estoque_minimo': 103, 'ativo': true, 'data_validade': '2027-01-18'},
+    
+    {'nome_produto': 'Placa do mouse com sensor calibrado', 'descricao': 'Produto semiacabado resultante de montagem parcial: Placa do mouse com sensor calibrado.', 'estoque_atual': 0, 'estoque_minimo': 119, 'ativo': true, 'data_validade': '2026-12-07'},
+    
+    {'nome_produto': 'Placa do teclado com firmware gravado', 'descricao': 'Produto semiacabado resultante de montagem parcial: Placa do teclado com firmware gravado.', 'estoque_atual': 0, 'estoque_minimo': 113, 'ativo': true, 'data_validade': '2026-08-10'},
+    
+    {'nome_produto': 'Placa do teclado soldada', 'descricao': 'Produto semiacabado resultante de montagem parcial: Placa do teclado soldada.', 'estoque_atual': 0, 'estoque_minimo': 53, 'ativo': true, 'data_validade': '2026-08-28'},
+    
+    {'nome_produto': 'Teclado compacto', 'descricao': 'Produto acabado pronto para venda ao consumidor: Teclado compacto.', 'estoque_atual': 0, 'estoque_minimo': 32, 'ativo': true, 'data_validade': '2026-05-17'},
+    
+    {'nome_produto': 'Teclado corporativo', 'descricao': 'Produto acabado pronto para venda ao consumidor: Teclado corporativo.', 'estoque_atual': -121, 'estoque_minimo': 54, 'ativo': true, 'data_validade': '2026-06-17'},
+    
+    {'nome_produto': 'Teclado gamer', 'descricao': 'Produto acabado pronto para venda ao consumidor: Teclado gamer.', 'estoque_atual': -230, 'estoque_minimo': 56, 'ativo': true, 'data_validade': '2026-07-15'},
+    
+    {'nome_produto': 'Teclado mecânico RGB', 'descricao': 'Produto acabado pronto para venda ao consumidor: Teclado mecânico RGB.', 'estoque_atual': -182, 'estoque_minimo': 25, 'ativo': true, 'data_validade': '2026-06-09'},
+    
+    {'nome_produto': 'Teclado sem fio', 'descricao': 'Produto acabado pronto para venda ao consumidor: Teclado sem fio.', 'estoque_atual': 0, 'estoque_minimo': 34, 'ativo': true, 'data_validade': '2026-04-27'},
+    
+    {'nome_produto': 'Teclas (keycaps) produzidas', 'descricao': 'Produto semiacabado resultante de montagem parcial: Teclas (keycaps) produzidas.', 'estoque_atual': 0, 'estoque_minimo': 71, 'ativo': true, 'data_validade': '2026-10-02'}
+    ];
---
+++ frontend/src/features/stock/styles/dataGridTheme.js
@@ -0,0 +1,17 @@
+// theme/components/dataGrid.js
+export const dataGridTheme = {
+    MuiDataGrid: {
+        styleOverrides: {
+
+            // Geral
+            root: {
+                border: 'none',
+                fontFamily: 'Roboto, sans-serif',
+                backgroundColor: '#989898',
+                borderRadius: '80px'
+            },
+
+        
+        },
+    },
+};
