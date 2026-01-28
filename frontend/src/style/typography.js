0e1c4aa205a55d5f5a721cdf4cb8cf81c3a5bc29
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-01-28 16:34:05 -0400

    style(table): DataGrid personalizado, com alguns bugs visuais

--- frontend/src/style/typography.js
+++ frontend/src/style/typography.js
@@ -1,1 +1,15 @@
 // typography.js
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+
+
+export const typography = {
+  fontFamily: 'Inter, sans-serif',
+
+  fontWeightLight: 400,
+  fontWeightRegular: 500,
+  fontWeightMedium: 600,
+  fontWeightBold: 700,
+};
