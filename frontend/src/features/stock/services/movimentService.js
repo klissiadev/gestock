845aecae52dfc004dcde3b781b12048efe858906
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-01-30 18:17:08 -0400

    refat(backend-table): Inicar conserto do backend

---
+++ frontend/src/features/stock/services/movimentService.js
@@ -0,0 +1,33 @@
+export async function fetchMovimentacoes(orderBy, isAsc, search) {
+    try {
+        console.log("Requisitando dados da tabela de movimentacoes com os parametros:");
+        console.log({ orderBy, isAsc, search });
+        const response = await fetch(
+            `http://localhost:8000/views/moviment`,
+            {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    "orderBy": orderBy,
+                    "isAsc": isAsc,
+                    "search": search,
+                })
+            }
+        );
+        
+
+        if (!response.ok) {
+            throw new Error(`Erro na API: ${response.status}`);
+        }
+
+        
+
+        const dados = await response.json();
+        return dados;
+
+    } catch {
+        return { error: "Nao foi possivel ler a tabela de movimentacoes" };
+    }
+};
