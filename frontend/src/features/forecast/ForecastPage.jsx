8859d1a36bebfe285cd625c72f350c84592712aa
Ana Klissia Furtado Martins <akfma.snf24@uea.edu.br>
2026-02-03 13:47:06 -0400

    fix(chat): Botão de atalho nas páginas

--- frontend/src/features/forecast/ForecastPage.jsx
+++ frontend/src/features/forecast/ForecastPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ExpandableIconButton from "../../components/ui/ExpandableIconButton.jsx";
+import ChatSvg from "../../assets/icon/iconChat.svg?react";
 
 const ForecastPage = () => {
   return (
-    <div>ForecastPage</div>
+    <div>ForecastPage
+      <ExpandableIconButton
+        icon={<ChatSvg width={20} height={20} />} 
+        origin="forecast"
+        initialMessage="Olá Minerva, me ajude na tela de previsão."
+      />
+    </div>
   )
 }
 
