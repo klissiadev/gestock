a2a4e569e4e346c11c43aa3e6101b5822b75bb85
Thais Carolina Braga de Magalhães <tcbdma.snf24@uea.edu.br>
2026-02-03 10:38:09 -0400

    refat(upload-box): Novo design de upload criado

---
+++ frontend/src/features/upload/components/TypeSelector.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FormControl, Select, MenuItem } from '@mui/material'
+import { typeSelector, menuItem } from '../styles/style'
+
+const TypeSelector = ({ value, onChange, name, options}) => {
+    return (
+        <FormControl fullWidth sx={{ borderColor: theme => theme.palette.common.black }}>
+            <Select
+                value={value}
+                size='small'
+                displayEmpty
+                onChange={(e) => onChange(name, e.target.value)}
+                sx={typeSelector}
+            >
+                {options.map((opt) => (
+                    <MenuItem sx={menuItem} key={opt.value} value={opt.value}>
+                        {opt.label}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+
+    )
+}
+
+export default TypeSelector
