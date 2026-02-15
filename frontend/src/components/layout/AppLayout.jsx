8446a41fd91eab985e5f2551cd6560aeabe983e1
Ana Klissia Furtado Martins <akfma.snf24@uea.edu.br>
2026-02-14 23:22:47 -0400

    feat(Header):Criação de context pro header

--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -1,11 +1,15 @@
+// frontend\src\components\layout\AppLayout.jsx
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import { ToastContainer } from "react-toastify";
 import { useState } from "react";
+import { HeaderProvider } from "../../HeaderContext";
+
 
 import SideBar from "./SideBar";
 import PageContainer from "./PageContainer";
 import Header from "./Header";
+
 
 
 export default function AppLayout() {
@@ -29,44 +33,46 @@ export default function AppLayout() {
     }
 
     return (
-        <Box display="flex" height="100vh" sx={{ overflow: "hidden" }}>
-            <ToastContainer />
-            <SideBar 
-                active={active}
-                onChange={handleChange}
-                expanded={expanded}
-                onToggle={toggleSidebar}
-            />
-            
-            {/* Conteúdo da página */}
-            <Box
-                sx={{
-                    flex: 1,
-                    display: "flex",
-                    flexDirection: "column",
-                    minWidth: 0,
-                    p: 2,
-                }}
-                >
-                {/* HEADER FIXO */}
-                <Box sx={{ flexShrink: 0 }}>
-                    <Header />
-                </Box>
-
-                {/* CONTEÚDO QUE SOBRA */}
+        <HeaderProvider>
+            <Box display="flex" height="100vh" sx={{ overflow: "hidden" }}>
+                <ToastContainer />
+                <SideBar 
+                    active={active}
+                    onChange={handleChange}
+                    expanded={expanded}
+                    onToggle={toggleSidebar}
+                />
+                
+                {/* Conteúdo da página */}
                 <Box
                     sx={{
-                    flex: 1,
-                    minHeight: 0,
-                    display: "flex",
-                    overflow: "hidden"
+                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                        minWidth: 0,
+                        p: 2,
                     }}
-                >
-                    <PageContainer>
-                    <Outlet />
-                    </PageContainer>
-                </Box>
-                </Box>
-        </Box>
+                    >
+                    {/* HEADER FIXO */}
+                    <Box sx={{ flexShrink: 0 }}>
+                        <Header />
+                    </Box>
+
+                    {/* CONTEÚDO QUE SOBRA */}
+                    <Box
+                        sx={{
+                        flex: 1,
+                        minHeight: 0,
+                        display: "flex",
+                        overflow: "hidden"
+                        }}
+                    >
+                        <PageContainer>
+                        <Outlet />
+                        </PageContainer>
+                    </Box>
+                    </Box>
+            </Box>
+        </HeaderProvider>
     );
 }
