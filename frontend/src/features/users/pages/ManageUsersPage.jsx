8f49baf811dac5ebc46b5fa5562040f1141666c2
Ana Klissia Furtado Martins <akfma.snf24@uea.edu.br>
2026-02-24 10:25:56 -0400

    style(users):Tabela de usuários

--- frontend/src/features/users/pages/ManageUsersPage.jsx
+++ frontend/src/features/users/pages/ManageUsersPage.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import { Box, Stack } from "@mui/material";
 import ManageUserBar from "../components/ManageUserBar";
+import TableUser from '../components/TableUser';
 import { useState } from 'react';
 
 const UsersPage = () => {
-    const [filters, setFilters] = useState({
-        searchTerm: "",
-        order: "",
-    });
+  const [filters, setFilters] = useState({
+      searchTerm: "",
+      order: "",
+  });
 
-    const handleFilterChange = (name, value) => {
-        setFilters((prev) => ({
-        ...prev,
-        [name]: value,
-        }));
+  const handleFilterChange = (name, value) => {
+      setFilters((prev) => ({
+      ...prev,
+      [name]: value,
+      }));
 
-        console.log("Filtro alterado:", name, value);
-    };
+      console.log("Filtro alterado:", name, value);
+  };
+
+  const rows = [
+    {
+      id: 1,
+      nome: "Nome Sobrenome",
+      email: "nome_sobrenome@gmail.com",
+      funcao: "Administrador",
+    },
+    {
+      id: 2,
+      nome: "Nome Sobrenome",
+      email: "nome_sobrenome@gmail.com",
+      funcao: "Técnico de Manutenção",
+    },
+  ];
+
   return (
     <Stack
       direction="column"
@@ -34,6 +51,8 @@ const UsersPage = () => {
         filters={filters}
         onFilterChange={handleFilterChange}
       />
+      
+    <TableUser rows={rows} selectedId={1} />
     </Stack>
   )
 }
