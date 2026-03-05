714b9e901bfa359eb1d9cf3c5e7ddd7fb98d48ad
Ana Klissia Furtado Martins <akfma.snf24@uea.edu.br>
2026-03-05 11:09:54 -0400

    fix(notification-import):Ajustando o disparo de notifiação de import e de ruptura de estoque

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,11 +1,11 @@
 # backend/main.py
 from fastapi import FastAPI
-from backend.routers import log_importacao_router  # Importe isso
+from backend.routers import log_importacao_router
 from backend.routers import views_router
 
 app = FastAPI()
 
-# Registre a rota
+
 app.include_router(log_importacao_router.router)
 app.include_router(views_router.router)
 
