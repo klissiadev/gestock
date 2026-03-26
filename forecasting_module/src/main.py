6c23e270629622f1da42c4b17bf9a828424dca94
Júlio Cleiton <jcsme.snf24@uea.edu.br>
2026-03-26 12:08:03 -0400

    fix(cors): configurar cors na main

--- forecasting_module/src/main.py
+++ forecasting_module/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from forecasting_module.services.model_loader import ModelRegistry
 from forecasting_module.services.anomaly_service import AnomalyService
 from forecasting_module.services.batch_detector import BatchDetector
@@ -6,6 +7,14 @@ from forecasting_module.services.anomaly_app_service import AnomalyAppService
 from forecasting_module.api.anomaly_router import create_anomaly_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # front
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL_PATH = "forecasting_module/ml_artifacts/modelos_vendas_completo.pkl"
 
